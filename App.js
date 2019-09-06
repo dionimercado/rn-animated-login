efa67b170cfbdf268b153bdfccaca62b1f818bd5
Dioni Mercado <me@dionimercado.com>
2019-09-06 00:43:28 -0400

    Small code improvement

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import React, { useState } from "react";
-import { Asset } from "expo-asset";
 import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
 import LoginScreen from "./src/screens/Login";
 
-const cacheImages = images => {
-  return images.map(image => {
-    if (typeof image === "string") {
-      return Image.prefetch(image);
-    } else {
-      return Asset.fromModule(image).downloadAsync();
-    }
-  });
-};
+const cacheImages = images =>
+  images.map(image =>
+    typeof image === "string"
+      ? Image.prefetch(image)
+      : Asset.fromModule(image).downloadAsync()
+  );
 
 export default () => {
   const [isReady, setIsReady] = useState(false);
